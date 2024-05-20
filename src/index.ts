858e53fc2cd3a31ed5805895dd98af3054fa667d
laponovsemen <simsbury65@gmail.com>
2024-05-20 14:50:36 +0400

    dd

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,16 @@ import { runDB } from './db/db'
 export const startApp = async () => {
     if(!await runDB()) {
         console.error("no db connection")
-        process.exit(1)
+        //process.exit(1)
     }
     app.listen(SETTINGS.PORT, async() => {
-        // ВСЕ Только title 
+        // ВСЕ Только title
         // console.log((await postCollection.find({},{projection: {title:1, _id:0}}).toArray()) as {title:string}[])
         // Только title применять для фильтра
         // console.log((await postCollection.find({title: /*'Other'*/ new RegExp('Ot','i')},{projection: {title:1, _id:0}}).toArray()) as {title:string}[])
         // Кол-во документов
         // console.log((await postCollection.countDocuments({title: /*'Other'*/ new RegExp('Ot','i')})))
-        
+
         console.log(`...server started on port ${SETTINGS.PORT}`)
     })
 }
